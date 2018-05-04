bd6a6456f0640ff23d12710cc0e60cd240934776
lixuhui <1064149@gwi.com.cn>
2018-05-04 11:34:03 +0800

    增加热敏打印机驱动分支

--- mechanism/dcmotor.h
+++
@@ -1,1 +0,0 @@
-../motor/dcmotor.h
--- motor/pwm-gwi.h
+++
@@ -1,6 +0,0 @@
-#ifndef ___PWM_GWI_H__
-#define ___PWM_GWI_H__
-
-extern int imx_pwm_set_callback(struct pwm_device *pwm,  irq_handler_t handler, void * dev_id);
-
-#endif
